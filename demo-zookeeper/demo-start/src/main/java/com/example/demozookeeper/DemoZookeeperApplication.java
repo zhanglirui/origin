aa8f44ad4c2a6694ff7b6026fb4f2e5e7300b2eb
wb-zlr386384 <wb-zlr386384@alibabapictures.com>
2020-12-07 16:11:58 +0800

    zookeeper客户端提交

--- demo-zookeeper/demo-start/src/main/java/com/example/demozookeeper/DemoZookeeperApplication.java
+++ demo-zookeeper/demo-start/src/main/java/com/example/demozookeeper/DemoZookeeperApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoZookeeperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoZookeeperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoZookeeperApplication.class, args);
+    }
 
 }
